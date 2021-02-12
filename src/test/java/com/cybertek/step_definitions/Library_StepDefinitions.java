62b8e3d53ac1f9106e46926152d32c9f7c06e982
gurhank <gurhankocyigit@gmail.com>
2021-02-12 13:20:14 -0600

    feature files

--- src/test/java/com/cybertek/step_definitions/Library_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Library_StepDefinitions.java
@@ -8,19 +8,19 @@ public class Library_StepDefinitions {
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-
+        System.out.println("User is on the login page.");
     }
 
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
-
+        System.out.println("User enters LIBRARIAN username");
     }
     @When("user enter librarian password")
     public void user_enter_librarian_password() {
-
+        System.out.println("User enters LIBRARIAN password");
     }
     @Then("user should see librarian in the dashboard")
     public void user_should_see_librarian_in_the_dashboard() {
-
+        System.out.println("User sees dashboard");
     }
 }
