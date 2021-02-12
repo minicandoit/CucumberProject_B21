c25b6d6effc26700030087977c9345f92cc718de
gurhank <gurhankocyigit@gmail.com>
2021-02-12 14:08:09 -0600

    cucumber day2

--- src/test/java/com/cybertek/step_definitions/Library_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Library_StepDefinitions.java
@@ -3,17 +3,18 @@ package com.cybertek.step_definitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Library_StepDefinitions {
 
-    @Given("user is on the login page")
-    public void user_is_on_the_login_page() {
-        System.out.println("User is on the login page.");
-    }
 
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
         System.out.println("User enters LIBRARIAN username");
+
+        //Intentionally failing this step
+        //Assert.fail("INTENTIONAL FAIL!!!!!!!");
+
     }
 
     @When("user enter librarian password")
@@ -25,4 +26,11 @@ public class Library_StepDefinitions {
     public void userShouldSeeDashboard() {
         System.out.println("User sees dashboard");
     }
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("User is on the login page.");
+    }
+
+
 }
