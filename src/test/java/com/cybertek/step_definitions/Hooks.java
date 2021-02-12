352b01e6c8765ce7171a3e96220fa09a3c0509e7
gurhank <gurhankocyigit@gmail.com>
2021-02-12 15:50:20 -0600

    cucumber day2

---
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -0,0 +1,19 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    @Before
+    public void setUpScenario(){
+        System.out.println("Setting up browser with further details...");
+    }
+
+    @After
+    public void tearDownScenario(){
+        System.out.println("Closing browser");
+        System.out.println("Take a screenshot");
+    }
+
+}
