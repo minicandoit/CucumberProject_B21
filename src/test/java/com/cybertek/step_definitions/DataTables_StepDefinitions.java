faace66503b15e3fbd801cc18ec63baa10b610a6
gurhank <gurhankocyigit@gmail.com>
2021-02-16 14:40:28 -0600

    cucumber day3

---
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -0,0 +1,13 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Then;
+
+public class DataTables_StepDefinitions {
+
+    @Then("User should see below words displayed")
+    public void user_should_see_below_words_displayed() {
+
+    }
+
+
+}
