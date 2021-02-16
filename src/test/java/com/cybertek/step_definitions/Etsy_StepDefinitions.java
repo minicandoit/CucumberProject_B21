4f19177dfbd8309154d606ff890c38b864635d57
gurhank <gurhankocyigit@gmail.com>
2021-02-16 12:06:04 -0600

    cucumber day3

--- src/test/java/com/cybertek/step_definitions/Etsy_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Etsy_StepDefinitions.java
@@ -1,13 +1,18 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.EtsySearchPage;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
+import io.cucumber.java.an.E;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class Etsy_StepDefinitions {
+
+    EtsySearchPage etsySearchPage = new EtsySearchPage();
 
     @Given("user is on the Etsy landing page")
     public void user_is_on_the_etsy_landing_page() {
@@ -27,6 +32,9 @@ public class Etsy_StepDefinitions {
 
     @When("user searches for {string}")
     public void userSearchesFor(String arg0) {
+
+        etsySearchPage.searchBox.sendKeys(arg0 + Keys.ENTER);
+
     }
 
     @Then("user should see {string} in the Etsy title")
