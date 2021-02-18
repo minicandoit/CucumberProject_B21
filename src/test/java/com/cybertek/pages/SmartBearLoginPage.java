ab402b4b83c2e0aa8c9fab8027fbe4fb0d1202aa
gurhank <gurhankocyigit@gmail.com>
2021-02-18 11:33:50 -0600

    cucumber day4

--- src/test/java/com/cybertek/pages/SmartBearLoginPage.java
+++ src/test/java/com/cybertek/pages/SmartBearLoginPage.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SmartBearLoginPage {
@@ -9,6 +11,14 @@ public class SmartBearLoginPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(id = "ctl00_MainContent_username")
+    public WebElement inputUsername;
+
+    @FindBy(id = "ctl00_MainContent_password")
+    public WebElement inputPassword;
+
+    @FindBy(id = "ctl00_MainContent_login_button")
+    public WebElement loginButton;
 
 
 }
