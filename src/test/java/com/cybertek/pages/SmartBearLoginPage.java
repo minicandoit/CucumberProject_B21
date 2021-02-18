3a9a8256d45526ca64a0bec937a56e12cdd751df
gurhank <gurhankocyigit@gmail.com>
2021-02-18 11:41:56 -0600

    cucumber day4

--- src/test/java/com/cybertek/pages/SmartBearLoginPage.java
+++ src/test/java/com/cybertek/pages/SmartBearLoginPage.java
@@ -1,5 +1,6 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,11 +21,24 @@ public class SmartBearLoginPage {
     @FindBy(id = "ctl00_MainContent_login_button")
     public WebElement loginButton;
 
+    //login method where we dont have to pass username, password
     public void loginToSmartBear(){
         inputUsername.sendKeys("Tester");
         inputPassword.sendKeys("test");
         loginButton.click();
     }
 
+    //overloaded version where we can pass username and password
+    public void loginToSmartBear(String username, String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+    }
 
+    //login method where we can pass data from configuration.properties
+    public void loginToSmartBear_Config(){
+
+        inputUsername.sendKeys(ConfigurationReader.getProperty(""));
+
+    }
 }
