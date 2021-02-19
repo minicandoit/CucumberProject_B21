2aad4b6c31112c287b5d7b23cd6b5c14d0049990
gurhank <gurhankocyigit@gmail.com>
2021-02-19 09:12:11 -0600

    cucumber day5

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -55,8 +55,21 @@ public class DataTables_StepDefinitions {
 
         BrowserUtils.titleVerification("Web Orders");
 
+    }
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+
+        Driver.getDriver().get("http://practice.cybertekschool.com/dropdown");
 
     }
 
+
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(io.cucumber.datatable.DataTable dataTable) {
+
+
+
+    }
 
 }
