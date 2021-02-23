56fbb4fcf2ec21c64786f1ad0998eb7d8e242bbc
gurhank <gurhankocyigit@gmail.com>
2021-02-23 13:34:30 -0600

    APACHE POI  SELENIUM IMPLEMENTATION

--- src/test/java/com/cybertek/tests/GasMileageTests.java
+++ src/test/java/com/cybertek/tests/GasMileageTests.java
@@ -1,6 +1,7 @@
 package com.cybertek.tests;
 
 import com.cybertek.pages.GasMileageCalculatorPage;
+import com.cybertek.utilities.Driver;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class GasMileageTests {
 
@@ -19,7 +21,18 @@ public class GasMileageTests {
     GasMileageCalculatorPage gasMileageCalculatorPage = new GasMileageCalculatorPage();
 
     @Test
-    public void gas_mileage_calculation(){
+    public void gas_mileage_calculation() throws IOException {
+
+        Driver.getDriver().get("https://www.calculator.net/gas-mileage-calculator.html");
+
+        //Get the path from Content Root
+        String path = "src/test/resources/testData/GasMileageTestData.xlsx";
+
+        //loading the file into inputStream object
+        fileInputStream = new FileInputStream(path);
+
+        //loading the workbook to the class
+        workbook = new XSSFWorkbook(fileInputStream);
 
     }
 
