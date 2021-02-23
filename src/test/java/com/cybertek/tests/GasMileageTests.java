bed0d37194a32927f75aed628d4f60c38d30f4ac
gurhank <gurhankocyigit@gmail.com>
2021-02-23 13:28:58 -0600

    APACHE POI  SELENIUM IMPLEMENTATION

--- src/test/java/com/cybertek/tests/GasMileageTests.java
+++ src/test/java/com/cybertek/tests/GasMileageTests.java
@@ -1,8 +1,22 @@
 package com.cybertek.tests;
 
+import com.cybertek.pages.GasMileageCalculatorPage;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class GasMileageTests {
+
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+    FileInputStream fileInputStream;
+    FileOutputStream fileOutputStream;
+    GasMileageCalculatorPage gasMileageCalculatorPage = new GasMileageCalculatorPage();
 
     @Test
     public void gas_mileage_calculation(){
