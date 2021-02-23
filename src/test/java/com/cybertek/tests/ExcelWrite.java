c24fff6791ff6fd4894807a756a324332ed4fc67
gurhank <gurhankocyigit@gmail.com>
2021-02-23 11:16:58 -0600

    APACHE POI Writing to Excel

--- src/test/java/com/cybertek/tests/ExcelWrite.java
+++ src/test/java/com/cybertek/tests/ExcelWrite.java
@@ -6,6 +6,9 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ExcelWrite {
 
     XSSFWorkbook workbook;
@@ -14,7 +17,20 @@ public class ExcelWrite {
     XSSFCell cell;
 
     @Test
-    public void excel_write(){
+    public void excel_write() throws IOException {
+
+        String path = "SampleData.xlsx";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        //workbook > sheet > row > cell
+
+        //Loading excel workbook into class
+        workbook = new XSSFWorkbook(fileInputStream);
+
+        //open the sheet using workbook object
+        sheet = workbook.getSheet("Employees");
+
 
     }
 }
