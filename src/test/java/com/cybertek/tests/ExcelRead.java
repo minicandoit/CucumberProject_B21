8d10d8a4ded0cb2ca39fd59063f2cae99f7f9a75
gurhank <gurhankocyigit@gmail.com>
2021-02-23 10:10:48 -0600

    APACHE POI Reading from Excel

--- src/test/java/com/cybertek/tests/ExcelRead.java
+++ src/test/java/com/cybertek/tests/ExcelRead.java
@@ -1,6 +1,11 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ExcelRead {
 
@@ -9,7 +14,16 @@ public class ExcelRead {
      */
 
     @Test
-    public void excel_read_test(){
+    public void excel_read_test() throws IOException {
+
+        String path = "SampleData.xlsx";
+
+        //To be able to read from excel file we need to load the file
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        //workbook > sheet > row > cell
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
 
     }
 
